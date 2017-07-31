a88449f7c5422320ee10d628093ee25cf3a6f28f
Makigumo <317106435@qq.com>
2017-07-31 08:10:47 +0800

    17-7-31

---
+++ Main Classes.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainClasses : MonoBehaviour {
+
+    public class character
+    {
+        private string name;
+        private string describe;
+        private int hp_ori, hp_cur_max, hp_cur;
+        private int speed_ori, speed_cur;
+        public character()
+        {
+            //初始化
+        
+        }
+        public character setName(string s)
+        {
+            name = s;
+            return this;
+        }
+        public character setDescribe(string s)
+        {
+            describe = s;
+            return this;
+        }
+        public character setOriginHp(int i)
+        {
+            hp_ori = i;
+            return this;
+        }
+        public character setOriginSpeed(int i)
+        {
+            speed_ori = i;
+            return this;
+        }
+        public character Creat()
+        {
+            //扫描牌组组，并初始化数值
+            hp_cur_max = hp_ori;
+            hp_cur = hp_cur_max;
+            speed_cur = speed_ori;
+            return this;
+        }
+        public string info()
+        {
+            string ans="";
+            ans = "名字：" + name + "\n"
+                + "介绍：" + describe + "\n"
+                + "血量：" + hp_cur + "/" + hp_cur_max + "\n"
+                + "速度：" + speed_cur + "\n";
+            //Debug.Log("hey");
+            return ans;
+        }
+        
+
+    }
+
+}
---
+++ MainCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;   //反射
+
+public class MainCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MainClasses.character myself = new MainClasses.character();
+        myself.setName("hhh").setDescribe("你们的爸爸")
+            .setOriginHp(2);
+        //用函数名调用函数的例子，注意到这个Invoke必须要对实例才起作用
+        Type type = typeof(MainClasses.character);
+        int speed_ori = 2;
+        object[] para = new object[] { speed_ori };
+        type.GetMethod("setOriginSpeed").Invoke(myself, para);
+        //
+        myself.Creat();
+        Debug.Log(myself.info());
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
